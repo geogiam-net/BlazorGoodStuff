05a3bdca41a4a8faa9d2c229efbb72d56c13bddc
George Baxter <morganaforlife@yandex.com>
2025-09-15 12:48:41 +0200

    Blazor web first commit  : D

---
+++ BlazorWeb/Pages/Contador.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWeb.Pages;
+
+public class ContadorBase : ComponentBase
+{
+    protected int currentCount = 0;
+
+    protected void IncrementCount()
+    {
+        currentCount++;
+    }
+}
---
+++ BlazorWeb/Pages/LoggerExample.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWeb.Pages;
+
+public class LoggerExampleBase : ComponentBase
+{
+    [Inject]
+    private ILogger<LoggerExampleBase> _logger { get; set; } = default!;
+
+    protected void LogDebug() => _logger.LogDebug("Hmmmm...");
+    protected void LogInformation() => _logger.LogInformation("Hi there!");
+    protected void LogWarning() => _logger.LogWarning("Some warning");
+    protected void LogError() => _logger.LogError("Some error");
+    protected void LogCritical() => _logger.LogCritical("Aaahhhhh!");
+    protected void LogConsole() => Console.WriteLine("This works too!");
+
+}
