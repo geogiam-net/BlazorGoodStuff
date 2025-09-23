a69d8cdc6f97721403bce7518265c69366302bcd
George Baxter <morganaforlife@yandex.com>
2025-09-23 12:26:00 +0200

    interop csharp

---
+++ BlazorWeb/Pages/Interoperability.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Diagnostics.Metrics;
+
+namespace BlazorWeb.Pages;
+
+public class InteroperabilityBase : ComponentBase
+{
+    [Inject]
+    public IJSRuntime jsRuntime { get; set; } = default!;
+
+    private Lazy<Task<IJSObjectReference>> moduleTask = default!;
+
+    protected string csharpResult = "";
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender) {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import",
+                "./scripts/tryInterop.js").AsTask());
+
+            DotNetObjectReference<InteroperabilityBase> objRef = DotNetObjectReference.Create(this);
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("putBlazorInstance", objRef);
+        }
+    }
+
+    protected async void InvokeJavascript()
+    {
+        var module = await moduleTask.Value;
+        await module.InvokeVoidAsync("showMessage", "Hello coming from #_#");
+    }
+
+    [JSInvokable]
+    public void InvokeCSharp()
+    {
+        csharpResult = "Hello coming from J_J";
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (moduleTask.IsValueCreated)
+        {
+            var module = await moduleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+}
---
+++ BlazorWeb/wwwroot/scripts/tryInterop.js
@@ -0,0 +1,16 @@
+﻿var blazorInstance = null;
+
+var showMessage = (message) => {
+    var elem = document.getElementById('jsResult');
+    elem.innerText = message;
+};
+
+var btClick = () => {
+    blazorInstance.invokeMethodAsync('InvokeCSharp');
+};
+
+var putBlazorInstance = (instance) => {
+    blazorInstance = instance;
+};
+
+export { showMessage, btClick, putBlazorInstance };
