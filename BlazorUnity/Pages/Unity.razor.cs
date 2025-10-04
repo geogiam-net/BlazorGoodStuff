4187ac4c82e7412635682eb5b763babdbd3d5775
George Baxter <morganaforlife@yandex.com>
2025-10-05 01:28:36 +0200

    unity in blazor javascript calls unity

---
+++ BlazorUnity/Pages/Unity.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorUnity;
+
+// probably we could lazy load   buildUrl + "/build.loader.js";
+// workaround, remember to rename /build2.data to /build2.data.json so it gets delivered by kestrel
+public class UnityBase : ComponentBase
+{
+    [Inject]
+    public IJSRuntime jsRuntime { get; set; } = default!;
+
+    private Lazy<Task<IJSObjectReference>> moduleTask = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+                "import",
+                "./scripts/unityHandler.js").AsTask());
+
+            DotNetObjectReference<UnityBase> objRef = DotNetObjectReference.Create(this);
+            var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("loadUnity", objRef);
+        }
+    }
+
+    protected async void InvokeJavascript()
+    {
+        var module = await moduleTask.Value;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (moduleTask.IsValueCreated)
+        {
+            var module = await moduleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+
+}
---
+++ BlazorUnity/wwwroot/scripts/unityHandler.js
@@ -0,0 +1,54 @@
+﻿// we need a destructor, remove script
+
+var loadUnity = () => {
+    var redButton = document.querySelector("#unity-red");
+    var blueButton = document.querySelector("#unity-blue");
+
+    var canvas = document.querySelector("#unity-canvas");
+    var buildUrl = "Build";
+    var loaderUrl = buildUrl + "/build2.loader.js";
+    var config = {
+        arguments: [],
+        dataUrl: buildUrl + "/build2.data.json",
+        frameworkUrl: buildUrl + "/build2.framework.js",
+        codeUrl: buildUrl + "/build2.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "UnityProject2",
+        productVersion: "0.1.0",
+        showBanner: () => { },
+    };
+
+    var script = document.createElement("script");
+    script.src = loaderUrl;
+
+    script.onload = () => {
+        createUnityInstance(canvas, config, (progress) => {
+            // document.querySelector("#unity-progress-bar-full").style.width = 100 * progress + "%";
+        }).then((unityInstance) => {
+            /*
+            document.querySelector("#unity-loading-bar").style.display = "none";
+            document.querySelector("#unity-fullscreen-button").onclick = () => {
+                unityInstance.SetFullscreen(1);
+            };
+            */
+
+            redButton.addEventListener('click', function () {
+                unityInstance.SendMessage('JavascriptHook', 'TintRed');
+                console.log('clicked red');
+            });
+
+            blueButton.addEventListener('click', function () {
+                unityInstance.SendMessage('JavascriptHook', 'TintBlue');
+                console.log('clicked blue');
+            });
+
+        }).catch((message) => {
+            alert(message);
+        });
+    };
+
+    document.body.appendChild(script);
+};
+
+export { loadUnity };
